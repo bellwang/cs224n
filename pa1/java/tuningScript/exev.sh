8297b9d812b3fec4f0af2096a3cbc35ec09e344a
Guan Wang <uwaterloozf@gmail.com>
2012-10-04 15:14:07 -0700

    update

--- pa1/java/tuningScript/exev.sh
+++ pa1/java/tuningScript/exev.sh
@@ -1,5 +1,5 @@
 #execute the code
 java -cp ~/cs224n/pa1/java/classes cs224n.assignments.WordAlignmentTester \
 -dataPath ~/cs224n \
--model cs224n.wordaligner.MyBestModel -language pa1-mt \
+-model cs224n.wordaligner.BaselineWordAligner -language pa1-mt \
 -outputAlignments ~/cs224n/pa1-mt/training/corpus.align
